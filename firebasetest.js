a802bf8a3d09288a055f07bdc027887afbed269c
unknown <campbell.nilsen@gmail.com>
2024-01-17 14:00:28 -0500

    rename

---
+++ firebasetest.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6Sl23ZOH7WHCvJHw2UKepZ6SZft_19jw",
+  authDomain: "eliotweb-e690e.firebaseapp.com",
+  projectId: "eliotweb-e690e",
+  storageBucket: "eliotweb-e690e.appspot.com",
+  messagingSenderId: "1076680288537",
+  appId: "1:1076680288537:web:83a561deee8eb42c20bd88"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
