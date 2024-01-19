c67c305af8f85d1ebe90f89c2564dcb487f23d00
unknown <campbell.nilsen@gmail.com>
2024-01-19 11:06:32 -0500

    better package now...

--- admin.js
+++ admin.js
@@ -1,31 +1,23 @@
-import {bookToChapterDict, chapterVerseDict} from './pythonscripts/chapterVerseDict.js';
+const express = require('express');
+const db = require('./db-connection/db');
+const cors = require('cors');
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js'
+
+const app = express();
+const PORT = 19054;
+app.use(cors());
+app.use(express.json())
+
+app.get("/api/get", (req,res)=>{
+    db.query("CREATE TABLE words (word_diac VARCHAR(255), ", (err,result)=>{
+        if(err) {
+        console.log(err)
+        } 
+    res.send(result)
+    });   
+});
     
-// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js'
 
-// Add Firebase products that you want to use
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
-import { collection, getDocs, doc, setDoc, getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB6Sl23ZOH7WHCvJHw2UKepZ6SZft_19jw",
-    authDomain: "eliotweb-e690e.firebaseapp.com",
-    projectId: "eliotweb-e690e",
-    storageBucket: "eliotweb-e690e.appspot.com",
-    messagingSenderId: "1076680288537",
-    appId: "1:1076680288537:web:83a561deee8eb42c20bd88"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-async function getBooks() {
-    const myCollection = collection(db, 'books');
-    const allBooks = await getDocs(myCollection);
-    return allBooks;
-}
 
 async function textFilePromise(fileName) {
     return fetch(fileName).then(response => response.text());
