7631382a81d1221f586f23da175de85805afcdd7
unknown <campbell.nilsen@gmail.com>
2024-01-19 15:36:58 -0500

    oops

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
-import getWords from "../backend/wordeditor.js";
+import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  let words = getWords();
-  let myString = "";
-  for (let word of getWords) {
-    myString += word + "<br>";
-  }
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {myString}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
+        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
+      </header>
     </div>
   );
 }
