52971aedb1d66a7d2af28108603764a86117c835
unknown <campbell.nilsen@gmail.com>
2024-01-19 17:07:56 -0500

    fixed pool?

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,6 @@
 const { Pool } = require("pg");
 
-// The secret connection string you copied earlier
-const dbURL = process.env.DATABASE_URL;
 
-const pool = new Pool({
-    dbURL,
-});
+const pool = new Pool(dbURL)
 
 module.exports = pool;
