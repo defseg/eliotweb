decd6b1b1ef6fb3b31853980852ac9eedd66fe0c
unknown <campbell.nilsen@gmail.com>
2024-01-19 16:10:06 -0500

    maybe everything needs to be a module?

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const { Pool } = require("pg");
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,7 @@ const wordEditor = require("./wordeditor.js");
 
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', dbURL);
+    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
