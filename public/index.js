ae817374ca7e89f43dc581a7764df76abecd1e6d
defseg <6988161+defseg@users.noreply.github.com>
2024-01-21 21:46:58 -0500

    Add increment button that talks to database

--- public/index.js
+++ public/index.js
@@ -1,5 +1,38 @@
-console.log("Hello world!")
+// TODO: This is bad. Don't use it.
+
+function wordToElement(word, count=0) {
+    const it = document.createElement('div')
+
+    const wordEl = document.createElement('span')
+    wordEl.innerText = word
+    it.appendChild(wordEl)
+
+    const countEl = document.createElement('span')
+    countEl.innerText = count
+    it.appendChild(countEl)
+
+    const buttonEl = document.createElement('button')
+    buttonEl.innerText = '+'
+    buttonEl.addEventListener("click", ev => {
+        let count = +(countEl.innerText)
+        count++
+        countEl.innerText = count
+
+        // PUT = update existing record. search term: "HTTP verbs"
+        fetch(`/words/${word}/increment`, {method: 'PUT'}).then(res => res.text()).then(res => console.log(res))
+    })
+    it.appendChild(buttonEl)
+
+    return it
+}
 
 function launchApp(el) {
-    el.innerText = "Hello world!"
+    el.innerText = "Loading words..."
+
+    fetch("/words").then(res => res.json()).then(res => {
+        el.innerText = ""
+        for (let item of res) {
+            el.appendChild(wordToElement(item.word, item.total_count))
+        }
+    }).catch(err => console.error(err))
 }
--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,14 @@ app.get('/dynamicContent', (req, res) => {
     res.send(`Hi! I'm some dynamic content! You loaded this page at millisecond ${new Date().getTime()} of the UNIX 年号.`)
 })
 
-app.get('/all_words', wrapAsync(async (req, res) => {
+app.get('/words', wrapAsync(async (req, res) => {
     const words = await client.query('SELECT * FROM words_diacritics')
     res.json(words.rows)
+}))
+
+app.put('/words/:word/increment', wrapAsync(async (req, res) => {
+    const update = await client.query('UPDATE words_diacritics SET total_count = total_count + 1 WHERE word = $1::text', [req.params.word])
+    res.json(update)
 }))
 
 // Default error handling middleware is fine for now
