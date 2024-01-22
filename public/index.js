3edff0c4816436f95aa013a9438092bd224835a5
defseg <6988161+defseg@users.noreply.github.com>
2024-01-21 19:44:09 -0500

    Start dummy frontend

---
+++ public/index.js
@@ -0,0 +1,5 @@
+console.log("Hello world!")
+
+function launchApp(el) {
+    el.innerText = "Hello world!"
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express"
+import path from "path"
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/dynamicContent', (req, res) => {
+    res.send(`Hi! I'm some dynamic content! You loaded this page at millisecond ${new Date().getTime()} of the UNIX 年号.`)
 })
+
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
